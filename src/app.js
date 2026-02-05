7abc58188fbbffb1271f2e322fa9c5c2fa8209c1
abhisek-arambh <abhisek02feb2004singh@gmail.com>
2026-02-05 23:40:43 +0530

    created a new express server

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/hello",(req , res) => {
+    res.end("Hello from the server");
+});
+app.use("/test",(req , res) => {
+    res.end("Welcome to the server");
+});
+app.use("/port",(req , res) => {
+    res.end("access to the server");
+});
+
+app.listen(7777, () =>{
+    console.log("server is sucessfully working");
+});
