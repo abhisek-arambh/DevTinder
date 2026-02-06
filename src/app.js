1577970469d236cb4a5bfd60344f322fa61ce977
abhisek-arambh <abhisek02feb2004singh@gmail.com>
2026-02-06 23:01:52 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -1,45 +1,49 @@
 const express = require("express");
 
 const app = express();
-//this will only handle get call to /user
-app.get("/user",(req , res) => {
-    res.send({firstName : "Abhisek" , lastName : "Singh" });
-});
+// //this will only handle get call to /user
+// app.get("/user",(req , res) => {
+//     res.send({firstName : "Abhisek" , lastName : "Singh" });
+// });
 
-app.post("/user",(req ,res) =>{
-    //saving data to db
-    res.send("your data is sucessfully saved to the database");
-})
+// app.post("/user",(req ,res) =>{
+//     //saving data to db
+//     res.send("your data is sucessfully saved to the database");
+// })
 
-app.put("/user", (req , res) =>{
-    res.send("i'm putting this data right now");
-});
+// app.put("/user", (req , res) =>{
+//     res.send("i'm putting this data right now");
+// });
 
-app.delete("/user", (req , res) =>{
-    //deleting data
-    res.send("your data is deleted sucessfully");
-});
+// app.delete("/user", (req , res) =>{
+//     //deleting data
+//     res.send("your data is deleted sucessfully");
+// });
 
-app.patch("/user", (req , res) => {
-    res.send("this data is patched");
-});
+// app.patch("/user", (req , res) => {
+//     res.send("this data is patched");
+// });
 
-app.head("/check", (req, res) => {
-  res.set("X-Server", "Express");
-  res.status(200).end();   // no response body
-});
+// app.head("/user", (req , res) => {
+//     res.send("this data is patched");
+//     res.send({objName: "thing" , strength:"5 fingers"});
+// });
 
-
-app.options("/user", (req , res) => {
-    res.send("the options are here");
-});
-//this will matches to all the http methods API call to /test
-app.use("/hello",(req , res) => {
+// app.options("/user", (req , res) => {
+//     res.send("the options are here");
+// });
+// //this will matches to all the http methods API call to /test
+app.use("/hello/:userid/:name/:password",(req , res) => {
+    console.log(req.params);
     res.end("Hello from the server");
 });
-app.use("/test",(req , res) => {
-    res.end("Welcome to the server");
-});
+// app.use("/hello",(req , res) => {
+//     console.log(req.query);
+//     res.end("Hello from the server");
+// });
+// app.use("/test",(req , res) => {
+//     res.end("Welcome to the server");
+// });
 // app.use("/",(req , res) => {
 //     res.end("access is given ");
 // });
