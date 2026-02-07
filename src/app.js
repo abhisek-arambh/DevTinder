8c916f8c7a0835b8e6966c8cd62a8dfd4b8e4bba
abhisek-arambh <abhisek02feb2004singh@gmail.com>
2026-02-07 23:23:45 +0530

    handling auth using middleware for all GET,POST,.........

--- src/app.js
+++ src/app.js
@@ -2,24 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use("/admin/GetAllData" , (req , res , next) => {
+app.use("/admin" , (req , res , next) => {
+    console.log("admin authorisation is getting checked");
     const token = "xyz";
     const isAdminAuthorised = token === "xyz";
-    if (isAdminAuthorised){
-        res.send("All data is clear");
+    if(!isAdminAuthorised){
+        res.status(401).send("Admin is not athorised");
     }else{
-        res.status(401).send("Admin is not atuthorised");
+        next();
     }
+})
+
+app.use("/admin/GetAllData" , (req , res) => {
+        res.send("All data is clear");
 });
 
-app.use("/admin/DeleteUser" , (req , res , next) => {
-    const token = "xyz";
-    const isAdminAuthorised = token === "xyz";
-    if (isAdminAuthorised){
+app.use("/admin/DeleteUser" , (req , res) => {
         res.send("Deleted a User");
-    }else{
-        res.status(401).send("Admin is not atuthorised");
-    }
 });
 
 app.listen(7777, () =>{
