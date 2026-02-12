9b25addf4009e38c0fd6f299661e186d5585857d
abhisek-arambh <abhisek02feb2004singh@gmail.com>
2026-02-13 00:09:40 +0530

    user authentication using middleware

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const { validateSignUpData } = require("./utils/validation");
 const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
+const {userAuth} = require("./middlewares/auth");
 
 app.use(express.json());
 app.use(cookieParser());
@@ -57,25 +58,10 @@ app.post("/login", async (req, res) => {
   }
 })
 
-app.get("/profile", async (req, res) => {
+app.get("/profile", userAuth, async (req, res) => {
   try{
-    const cookies = req.cookies;
-  const {token} = cookies;
-
-  if(!token){
-    throw new Error("Unauthorised");
-  }
-
-  const decodedToken = await jwt.verify(token, "secretKey");
-  const {userId} = decodedToken;
-
-  const user = await User.findById(userId);
-  if(!user){
-    throw new Error("user not found");
-  }
-
-  // res.send("profile pagge");
-  res.send(user);
+    const user = req.user;
+    res.send(user);
   }catch (err) {
     res.status(404).send("Something went wrong");
   }
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,25 +1,25 @@
-const adminAuth =(req , res , next) => {
-    console.log("admin authorisation is getting checked");
-    const token = "xyz";
-    const isAdminAuthorised = token === "xyz";
-    if(!isAdminAuthorised){
-        res.status(401).send("Admin is not athorised");
-    }else{
-        next();
-    }
-}
-const userAuth =(req , res , next) => {
-    console.log("admin authorisation is getting checked");
-    const token = "xyz";
-    const isAdminAuthorised = token === "xyz";
-    if(!isAdminAuthorised){
-        res.status(401).send("Admin is not athorised");  
-    }else{
-        next();
-    }
-}
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
+const userAuth = async (req, res, next) => {
+    try{
+        
+        const {token} = req.cookies;
+        if(!token){
+            throw new Error("Token is not valid");
+        }
+        const decodeObj = await jwt.verify(token, "secretKey");
+        const{userId} = decodeObj;
+        const user = await User.findById(userId);
+        if(!user){
+            throw new Error("Unauthorised");
+        }
+        req.user = user;
+        next();
+    }catch(err){
+        res.status(400).send("Error: " + err.message);
+    }
+}
 module.exports = {
-    adminAuth,
     userAuth,
 }
